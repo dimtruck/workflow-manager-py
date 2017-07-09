452d665ac974eab1468f56d2da9c31e77de4136c
dimtruck <dimitry.ushakov@rackspace.com>
2017-07-08 21:43:52 -0500

    Clean up setup.py and add readme and license

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='workflow_manager',
       version='0.1',
-      description='Workflow manager for tasks',
+      description='Wizard-link workflow manager',
+      long_description=readme(),
       url='http://github.com/dimtruck/workflow-manager-py',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Workflow Manager :: Wizard',
+      ],
       author='Dimitry Ushakov',
       author_email='me@dimitryushakov.com',
       license='MIT',
